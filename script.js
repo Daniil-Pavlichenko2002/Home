d5306f0bfdac6d1a387cf740c23d4d6a53c5b664
Daniil-Pavlichenko2002 <pavlichenko.02@internet.ru>
2023-04-10 17:54:40 +0300

    Все готово

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.querySelector('.header__burger').addEventListener('click', function() {
+    document.querySelector('.header__burger').classList.toggle('active');
     document.querySelector('.header__meny').classList.toggle('open');
     document.querySelector('body').classList.toggle('noscroll');
     document.querySelector('.footer').classList.toggle('footer__fixed');
     document.querySelector('.services__button').classList.toggle('button__fixed');
+
     // if(document.footer.style.display = 'block'){
     //     document.footer.style.overflow = 'hidden'
     // } else {
@@ -10,3 +12,7 @@ document.querySelector('.header__burger').addEventListener('click', function() {
     // }
     // document.querySelector('.wrapper').classList.toggle('shadow')
     })
+// document.querySelector('body').addEventListener('click', function(){
+//     document.header__meny.style.display = 'none'
+//     document.querySelector('.header__meny').classList.add('body__click');
+// })
